707308d325b9e2b1200c6db6e3ea6d94db7f36dd
juhyun88 <tls839@naver.com>
2025-05-11 22:22:37 +0900

    tailwindcss 적용용

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,30 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // ✅ Vite + React + TS에 꼭 필요
+    "./src/**/*.{js,ts,jsx,tsx,css,html}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primery: "var(--Primery)",
+        mainBrownLight: "var(--MainBrownLight)",
+        textColor: "var(--text-color)",
+        light: "var(--light-color)",
+      },
+      fontFamily: {
+        thin: ["'Paperlogy 1 Thin'", "sans-serif"],
+        extralight: ["'Paperlogy 2 ExtraLight'", "sans-serif"],
+        light: ["'Paperlogy 3 Light'", "sans-serif"],
+        regular: ["'Paperlogy 4 Regular'", "sans-serif"],
+        medium: ["'Paperlogy 5 Medium'", "sans-serif"],
+        semibold: ["'Paperlogy 6 SemiBold'", "sans-serif"],
+        bold: ["'Paperlogy 7 Bold'", "sans-serif"],
+        mango: ["'MangoDdobak-B'", "sans-serif"],
+      },
+      borderRadius: {
+        DEFAULT: "var(--border-radius)",
+      },
+    },
   },
   plugins: [],
 }
