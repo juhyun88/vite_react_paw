da4768d586faf57fdf2d2c903176cdbcd0a4040e
juhyun88 <tls839@naver.com>
2025-06-02 15:47:59 +0900

    서브페이지 틀 제작 서비스부분

--- src/features/funeral/Funeral.tsx
+++ src/features/funeral/Funeral.tsx
@@ -3,21 +3,25 @@ import FuneralProcedure from "./pages/FuneralProcedure";
 import NormalFuneral from "./pages/NormalFuneral";
 import StoneFuneral from "./pages/StoneFuneral";
 
-type FuneralComponentPath = "/procedures" | "/price" | "/stoneprice";
-
-interface FuneralProps {
+type FuneralProps = {
   className?: string;
-  componentNm: FuneralComponentPath;
-}
+  componentNm: string; // 라우터 값으로 
+};
+
+const COMPONENT_MAP = {
+  procedures: FuneralProcedure as React.FC<{ className?: string }>,
+  price: NormalFuneral as React.FC<{ className?: string }>,
+  stoneprice: StoneFuneral as React.FC<{ className?: string }>,
+};
+
 
 const Funeral: React.FC<FuneralProps> = ({ className = "", componentNm }) => {
-  const safeComponentClass = componentNm.replace(/\//g, "");
+
+  const Component = COMPONENT_MAP[componentNm as keyof typeof COMPONENT_MAP] || null;
 
   return (
     <div className={`relative flex flex-col ${className}`}>
-      {componentNm === "/procedures" && <FuneralProcedure className={safeComponentClass} />}
-      {componentNm === "/price" && <NormalFuneral className={safeComponentClass} />}
-      {componentNm === "/stoneprice" && <StoneFuneral className={safeComponentClass} />}
+      {Component && <Component className="" />}
     </div>
   );
 };
---
+++ src/features/services/pages/Boneservice.tsx
@@ -0,0 +1,11 @@
+
+
+function Boneservice({className} : {className:string}) {
+    return (
+        <div className={className}>
+            봉안당(산골)
+        </div>
+    );
+}
+
+export default Boneservice;
