288570164b1191d21c80e642c270e66f76f7039d
juhyun88 <tls839@naver.com>
2025-06-02 13:31:36 +0900

    서브페이지 제작중

--- src/features/brand/pages/BrandIntro.tsx
+++ src/features/brand/pages/BrandIntro.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-function BrandIntro() {
+function BrandIntro({className} : {className:string}) {
     return (
-        <div>
-            
+        <div className={className}>
+            브랜드 컨텐츠 넣어주세요.
         </div>
     );
 }
---
+++ src/features/funeral/pages/NormalFuneral.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NormalFuneral({className} : {className:string}) {
+    return (
+        <div className={className}>
+            일반 비용
+        </div>
+    );
+}
+
+export default NormalFuneral;
